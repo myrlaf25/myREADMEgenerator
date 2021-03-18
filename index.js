5875faf8284e4d155a40f8d6a5192301c75437ad
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-03-17 18:37:48 -0700

    first changes

---
+++ index.js
@@ -0,0 +1,60 @@
+// TODO: Include packages needed for this application
+//node modules
+const fs = require("fs")
+//NPM modules
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+inquirer
+  .prompt([
+    {
+        //these are an array of question objects
+      type: 'input',
+      message: 'What is the project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'What was your motivation?',
+      name: 'motivation',
+    },
+    {
+      type: 'input',
+      message: 'Why did you build this project?',
+      name: 'build',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your app?',
+        name: 'installation',
+      },
+    {
+        
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'languages',
+      choices:["English", "Spanish", "French", "German"]
+    },
+    {
+      type: 'list',
+      message: 'What license did you use?',
+      name: 'license',
+      choices: ["MIT License", "Apache License 2.0", "the Unlicense"]
+    },
+    {
+        type: 'input',
+        message: 'GitHub username:',
+        name: 'github',
+      },
+  ])
+
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
